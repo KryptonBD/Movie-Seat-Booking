01bd37299b478c90e30380ee82a9531c4f648d26
Foysol Ahmed <foysol.sahi@gmail.com>
2020-07-06 12:05:29 +0600

    done

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,7 +6,7 @@ const movieSelect = document.getElementById("movie");
 
 let ticketPrice = +movieSelect.value;
 
-function setMovieDataHandler(movieIndex, moviePrice){
+function setMovieDataHandler(movieIndex, moviePrice) {
     localStorage.setItem("selectedMovieIndex", movieIndex);
     localStorage.setItem("selectedMoviePrice", moviePrice);
 }
@@ -25,39 +25,39 @@ function updateSelectedHandler() {
 }
 
 //Populating  UI with Saved Data
-(function(){
+(function () {
     const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
     const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
     const selectedMoviePrice = localStorage.getItem("selectedMoviePrice");
 
-    if(selectedSeats != null && selectedSeats.length > 0) {
-        seats.forEach((seat, index )=>{
-            if(selectedSeats.indexOf(index) > -1) {
+    if (selectedSeats != null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
                 seat.classList.add("seat--selected");
             }
         })
     }
 
-    if(selectedMovieIndex != null) {
+    if (selectedMovieIndex != null) {
         movieSelect.selectedIndex = +selectedMovieIndex;
     }
 
-    if(selectedMoviePrice != null) {
+    if (selectedMoviePrice != null) {
         ticketPrice = +selectedMoviePrice;
     }
     updateSelectedHandler();
 })();
 
 //Movie Selection
-movieSelect.addEventListener("change", e =>{
+movieSelect.addEventListener("change", e => {
     ticketPrice = +e.target.value;
     setMovieDataHandler(e.target.selectedIndex, e.target.value);
     updateSelectedHandler();
 })
 
 //Seat Selection
-container.addEventListener("click", e=>{
-    if(e.target.classList.contains('seat') && !e.target.classList.contains('seat--occupied')){
+container.addEventListener("click", e => {
+    if (e.target.classList.contains('seat') && !e.target.classList.contains('seat--occupied')) {
         e.target.classList.toggle("seat--selected");
         updateSelectedHandler();
     }
